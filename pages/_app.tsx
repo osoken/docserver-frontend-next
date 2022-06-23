2dcf1ea5dc3f4ed2665f6ff6054c3db628eb5388
osoken <osoken.devel@outlook.jp>
2022-06-24 00:21:05 +0900

    feat: introduce mui

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,17 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { CacheProvider } from '@emotion/react'
+import lightTheme from '../styles/theme/lightTheme'
+import { ThemeProvider } from '@mui/material'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <CacheProvider value={pageProps.clientSideEmotionCache}>
+      <ThemeProvider theme={lightTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </CacheProvider>
+  )
 }
 
 export default MyApp
