5dbad3c2522d191e761d65484cf74e6cb567ac37
osoken <osoken.devel@outlook.jp>
2022-06-26 22:58:20 +0900

    feat: make header responsive to scroll

--- components/header.tsx
+++ components/header.tsx
@@ -1,14 +1,33 @@
-import { Typography, AppBar, Toolbar } from '@mui/material';
+import { Typography, AppBar, Toolbar, useScrollTrigger, Slide } from '@mui/material';
+
+interface Props {
+  window?: () => Window;
+  children: React.ReactElement;
+}
+
+function HideOnScroll(props: Props) {
+  const { children, window } = props;
+  const trigger = useScrollTrigger({
+    target: window ? window() : undefined,
+  });
+  return (
+    <Slide appear={false} direction="down" in={!trigger}>
+      {children}
+    </Slide>
+  );
+}
 
 function Header() {
   return (
-    <AppBar position="static">
-      <Toolbar>
-        <Typography variant="h6" sx={{ flexGrow: 1 }}>
-          Docserver
-        </Typography>
-       </Toolbar>
-    </AppBar>
+    <HideOnScroll>
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" sx={{ flexGrow: 1 }}>
+            Docserver
+          </Typography>
+        </Toolbar>
+      </AppBar>
+    </HideOnScroll>
   );
 }
 
