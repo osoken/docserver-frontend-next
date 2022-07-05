0d40e9210cab526d0a2a52d97bd3c85975948ad3
osoken <osoken.devel@outlook.jp>
2022-07-06 00:37:50 +0900

    fix: remove id

--- components/passwordInput.tsx
+++ components/passwordInput.tsx
@@ -3,10 +3,8 @@ import { TextField } from "@mui/material";
 export default function PasswordInput() {
   
   return <TextField
-    id="outlined-password-input"
     label="Password"
     type="password"
     autoComplete="current-password"
   />
-
 }
